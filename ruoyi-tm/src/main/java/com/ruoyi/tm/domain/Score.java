1ab07c4b2ce844f0faa84cfcee4d13a797c7496f
clement <15089246141@163.com>
2024-06-22 13:59:33 +0800

    修复学生信息显示bug

--- ruoyi-tm/src/main/java/com/ruoyi/tm/domain/Score.java
+++ ruoyi-tm/src/main/java/com/ruoyi/tm/domain/Score.java
@@ -5,6 +5,9 @@ import com.ruoyi.common.core.domain.BaseEntity;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+
+// 注意，这个弃用了，但是暂时先放这里以备不时之需
+// 最新的成绩信息请看score_info表单
 public class Score extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
