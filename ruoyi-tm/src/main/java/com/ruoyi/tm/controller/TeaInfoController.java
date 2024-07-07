291c012a73474088f47b8dfa55c42f2882c097ac
clement <15089246141@163.com>
2024-07-07 22:17:24 +0800

    新增教师导入功能

--- ruoyi-tm/src/main/java/com/ruoyi/tm/controller/TeaInfoController.java
+++ ruoyi-tm/src/main/java/com/ruoyi/tm/controller/TeaInfoController.java
@@ -3,18 +3,13 @@ package com.ruoyi.tm.controller;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ruoyi.common.core.domain.model.LoginUser;
+import com.ruoyi.tm.domain.CourseInfo;
 import com.ruoyi.tm.service.IClassInfoService;
 import com.ruoyi.tm.service.ICourseInfoService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
@@ -23,6 +18,7 @@ import com.ruoyi.tm.domain.TeaInfo;
 import com.ruoyi.tm.service.ITeaInfoService;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.page.TableDataInfo;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 教师信息Controller
@@ -131,5 +127,18 @@ public class TeaInfoController extends BaseController
         return success(classInfoService.selectClassByTeacherId(id));
     }
 
-
+    /**
+     * 导入Teacher
+     */
+    @PostMapping("/importData")
+    @ResponseBody
+    public AjaxResult importData(MultipartFile file, boolean updateSupport) throws Exception
+    {
+        ExcelUtil<TeaInfo> util = new ExcelUtil<TeaInfo>(TeaInfo.class);
+        List<TeaInfo> teaList = util.importExcel(file.getInputStream());
+        LoginUser loginUser = getLoginUser();
+        String operName = loginUser.getUsername();
+        String message = teaInfoService.importTea(teaList, updateSupport, operName);
+        return AjaxResult.success(message);
+    }
 }
--- ruoyi-tm/src/main/java/com/ruoyi/tm/service/impl/TeaInfoServiceImpl.java
+++ ruoyi-tm/src/main/java/com/ruoyi/tm/service/impl/TeaInfoServiceImpl.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.domain.entity.SysUser;
+import com.ruoyi.common.exception.ServiceException;
 import com.ruoyi.common.utils.SecurityUtils;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.common.utils.bean.BeanValidators;
 import com.ruoyi.system.mapper.SysUserMapper;
 import com.ruoyi.system.service.impl.SysUserServiceImpl;
 import com.ruoyi.tm.domain.CourseInfo;
@@ -15,6 +18,10 @@ import com.ruoyi.tm.domain.TeaInfo;
 import com.ruoyi.tm.service.ITeaInfoService;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 /**
  * 教师信息Service业务层处理
  * 
@@ -22,8 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @date 2024-07-06
  */
 @Service
-public class TeaInfoServiceImpl implements ITeaInfoService 
-{
+public class TeaInfoServiceImpl implements ITeaInfoService {
     @Autowired
     private TeaInfoMapper teaInfoMapper;
 
@@ -35,38 +41,35 @@ public class TeaInfoServiceImpl implements ITeaInfoService
 
     /**
      * 查询教师信息
-     * 
+     *
      * @param id 教师信息主键
      * @return 教师信息
      */
     @Override
-    public TeaInfo selectTeaInfoById(Long id)
-    {
+    public TeaInfo selectTeaInfoById(Long id) {
         return teaInfoMapper.selectTeaInfoById(id);
     }
 
     /**
      * 查询教师信息列表
-     * 
+     *
      * @param teaInfo 教师信息
      * @return 教师信息
      */
     @Override
-    public List<TeaInfo> selectTeaInfoList(TeaInfo teaInfo)
-    {
+    public List<TeaInfo> selectTeaInfoList(TeaInfo teaInfo) {
         return teaInfoMapper.selectTeaInfoList(teaInfo);
     }
 
     /**
      * 新增教师信息
-     * 
+     *
      * @param teaInfo 教师信息
      * @return 结果
      */
     @Override
     @Transactional
-    public int insertTeaInfo(TeaInfo teaInfo)
-    {
+    public int insertTeaInfo(TeaInfo teaInfo) {
         SysUser sysUser = new SysUser();
         sysUser.setUserName(teaInfo.getName());    // 设置用户名
         sysUser.setNickName(teaInfo.getName());     // 给sys_user昵称赋值
@@ -77,23 +80,21 @@ public class TeaInfoServiceImpl implements ITeaInfoService
         sysUser.setPassword("123456");  // 默认密码为123456
         sysUser.setPassword(SecurityUtils.encryptPassword(sysUser.getPassword()));
         sysUserMapper.insertTeacherUser(sysUser);
-        if(teaInfo.getType().equals("教师")){
+        if (teaInfo.getType().equals("教师")) {
             // 插入用户的对应角色，便于进行权限控制
-            Long sid = (long)101;   // 101为教师权限编号
+            Long sid = (long) 101;   // 101为教师权限编号
             Long[] roleIds = {sid};
             sysUserService.insertUserRole(sysUser.getUserId(), roleIds);
             teaInfo.setType("教师");
-        }
-        else if(teaInfo.getType().equals("教务老师")){
+        } else if (teaInfo.getType().equals("教务老师")) {
             // 插入用户的对应角色，便于进行权限控制
-            Long sid = (long)102;   // 102为教务老师权限编号
+            Long sid = (long) 102;   // 102为教务老师权限编号
             Long[] roleIds = {sid};
             sysUserService.insertUserRole(sysUser.getUserId(), roleIds);
             teaInfo.setType("教务老师");
-        }
-        else{
+        } else {
             // 插入用户的对应角色，便于进行权限控制
-            Long sid = (long)103;   // 103为班主任权限编号
+            Long sid = (long) 103;   // 103为班主任权限编号
             Long[] roleIds = {sid};
             sysUserService.insertUserRole(sysUser.getUserId(), roleIds);
             teaInfo.setType("班主任");
@@ -105,38 +106,85 @@ public class TeaInfoServiceImpl implements ITeaInfoService
 
     /**
      * 修改教师信息
-     * 
+     *
      * @param teaInfo 教师信息
      * @return 结果
      */
     @Override
-    public int updateTeaInfo(TeaInfo teaInfo)
-    {
+    public int updateTeaInfo(TeaInfo teaInfo) {
         return teaInfoMapper.updateTeaInfo(teaInfo);
     }
 
     /**
      * 批量删除教师信息
-     * 
+     *
      * @param ids 需要删除的教师信息主键
      * @return 结果
      */
     @Override
-    public int deleteTeaInfoByIds(Long[] ids)
-    {
+    public int deleteTeaInfoByIds(Long[] ids) {
         return teaInfoMapper.deleteTeaInfoByIds(ids);
     }
 
     /**
      * 删除教师信息信息
-     * 
+     *
      * @param id 教师信息主键
      * @return 结果
      */
     @Override
-    public int deleteTeaInfoById(Long id)
-    {
+    public int deleteTeaInfoById(Long id) {
         // 保留原来的信息，只修改对应的del_flag
         return sysUserMapper.deleteUserById(id);
     }
+
+    /**
+     * 批量导入教师信息
+     *
+     * @param teaList       课程信息集合
+     * @param updateSupport 是否更新支持，如果已存在，则进行更新数据
+     * @return 结果
+     */
+    @Override
+    public String importTea(List<TeaInfo> teaList, boolean updateSupport, String operName) {
+        if (StringUtils.isNull(teaList) || teaList.size() == 0)
+        {
+            throw new ServiceException("导入教师数据不能为空！");
+        }
+        int successNum = 0;
+        int failureNum = 0;
+        StringBuilder successMsg = new StringBuilder();
+        StringBuilder failureMsg = new StringBuilder();
+        for (TeaInfo teacher : teaList)
+        {
+            try
+            {
+                ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+                Validator validator = factory.getValidator();
+                BeanValidators.validateWithException(validator, teacher);
+                teacher.setCreateBy(operName);
+                this.insertTeaInfo(teacher);
+                successNum++;
+                successMsg.append("<br/>" + successNum + "、教师 " + teacher.getName() + " 导入成功");
+
+            }
+            catch (Exception e)
+            {
+                failureNum++;
+                String msg = "<br/>" + failureNum + "、教师 " + teacher.getName() + " 导入失败：";
+                failureMsg.append(msg + e.getMessage());
+            }
+        }
+        if (failureNum > 0)
+        {
+            failureMsg.insert(0, "很抱歉，导入失败！共 " + failureNum + " 条数据格式不正确，错误如下：");
+            throw new ServiceException(failureMsg.toString());
+        }
+        else
+        {
+            successMsg.insert(0, "恭喜您，数据已全部导入成功！共 " + successNum + " 条，数据如下：");
+        }
+        return successMsg.toString();
+    }
 }
+
