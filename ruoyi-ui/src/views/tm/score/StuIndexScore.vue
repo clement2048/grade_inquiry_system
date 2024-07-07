bf1ff2e3dc382d8d725e1d42a44348dd22e23bfe
Ling Wang <1635204627@qq.com>
2024-07-07 19:41:57 +0800

    学生首页更多修改

--- ruoyi-ui/src/views/tm/score/StuIndexScore.vue
+++ ruoyi-ui/src/views/tm/score/StuIndexScore.vue
@@ -6,13 +6,18 @@
           <div class="center"><span>成绩</span></div>
         </el-col>
         <el-col :span="6">
-          <div class="right"><el-button type="text">更多</el-button></div>
+          <div class="right">
+            <router-link to="/tm/score">
+              <el-button type="text">更多</el-button>
+            </router-link >
+          </div>
         </el-col>
       </el-row>
     </div>
     <el-table  :data="scoreList" >
       <el-table-column label="课程名" align="center" prop="courseName" />
       <el-table-column label="总成绩" align="center" prop="average" />
+      <el-table-column label="排名" align="center" prop="rank" />
     </el-table>
   </div>
 
